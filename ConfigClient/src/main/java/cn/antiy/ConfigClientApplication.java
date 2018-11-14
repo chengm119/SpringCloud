66e22060a91014179534922632cee572f8e6f1e6
chengm119 <chengm119@163.com>
2018-11-14 17:36:15 +0800

    haha

--- ConfigClient/src/main/java/cn/antiy/ConfigClientApplication.java
+++ ConfigClient/src/main/java/cn/antiy/ConfigClientApplication.java
@@ -3,8 +3,6 @@ package cn.antiy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
